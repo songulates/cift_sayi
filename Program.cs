47784efd4a352194c51a6552500e7a4dfe0c7fab
songulates <songl.ates.02@gmail.com>
2022-07-22 16:40:47 +0300

    odev

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace proje_odev // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kaç Adet sayı girmek istersin: ");
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            {
+                List<int> ciftSayilar = new List<int>();
+                for (int i = 0; i < n; i++)
+                {
+                    int sayi = Convert.ToInt32(Console.ReadLine());
+                    if (sayi % 2 == 0)
+                    {
+                        ciftSayilar.Add(sayi);
+                    }
+                }
+
+                foreach (var sayi in ciftSayilar)
+                {
+                    Console.Write(sayi + " ");
+                }
+
+            }
+            Console.Read();
+        }
+
+    }
+}
